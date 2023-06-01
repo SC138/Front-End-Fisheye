137946b81a2807a39b4d6cec9bd00e0ba9eaa924
Thomas Chevron <chevron.thomas33@gmail.com>
2023-06-01 10:40:19 +0200

    fin de la modal en JS

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,6 +8,7 @@ async function dataPhotographers(file){
     return data;
 };
 
+
 function findPhotographer(photographers, id){
     const photographer = photographers.find(photographer => photographer.id === id);
     return photographer;
@@ -20,16 +21,42 @@ function headerPhotographer(photographer){
 
     // ajout d'un attribut qui permet de cibler dynamiquement le chemin de picture
     img.setAttribute("src", `../assets/photographers/${photographer.portrait}`);
+
     // ajout de alt pour l'accéssibilité en ciblant dynamiquement le nom du photographe
     img.alt=`Portrait de ${photographer.name}`;
 
+    // création d'un élément h2
+    const h2 = document.createElement( 'h2' );
+    // je stipule que le h2 doit afficher name
+    h2.textContent = `${photographer.name}`;
+    // j'ajoute une class pour le css
+    h2.classList.add("name_photographer");
+
+    // création d'un élément p
+    const location = document.createElement( 'p' );
+    // je stipule que le p cible dynamiquement city et country en fonction du photographe
+    location.textContent = `${photographer.city}, ${photographer.country}`;
+    // j'ajoute une class pour le css
+    location.classList.add("location");
+
+    // création d'un élément p 
+    const catchline = document.createElement ('p');
+    // je stipule que le p doit afficher la tagline
+    catchline.textContent = `${photographer.tagline}`;
+    // j'ajoute une class pour le css
+    catchline.classList.add("catchline");
+
     photographHeader.appendChild(img);
+    photographHeader.appendChild(h2);
+    photographHeader.appendChild(location);
+    photographHeader.appendChild(catchline);
 };
 
 
 
-// const photographerName = document.querySelector(".photographer_name");
-// photographerName.textContent = photographer.name;
+
+
+
 
 async function init(){
     const data = await dataPhotographers("../../data/photographers.json");
@@ -37,9 +64,12 @@ async function init(){
     const urlParams = new URLSearchParams(window.location.search);
     const photographerID = parseInt (urlParams.get("id"));
     const photographer = findPhotographer(photographers, photographerID);
+    const photographerName = document.querySelector(".photographer_name");
+    photographerName.textContent = photographer.name;
     headerPhotographer(photographer);
 };
 
 
 
 init();
+
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,62 @@
-const modal = document.getElementById("contact_modal");
+const formElement = document.querySelector("form");
 const contact_button = document.querySelector(".contact_button");
 const close_button = document.querySelector(".close_button");
-
+const contact_modal = document.getElementById("contact_modal");
 
 // ouverture modal form
 function displayModal() {
-	modal.style.display = "block";
+	contact_modal.style.display = "block";
 }
+
 
 // fermeture modal
 function closeModal() {
-    modal.style.display = "none";
+    contact_modal.style.display = "none";
 }
 
+// variable isSubmited sur false
+let isSubmited = false;
 
+// je met un écouteur d'évévement sur le bouton envoyer(submit) sur l'élément formElement
+// quand l'événement se produit, la fonction fléchée s'éxécute 
+formElement.addEventListener("submit", (event) => {
+    // première instruction, event.preventDefault() permet d'empêcher le rechargement de la page par défaut au moment du submit du formulaire
+    event.preventDefault();
+    // récupération des inputs dans le HTML via getElementById en ajoutant le .value
+    // .value sert à accéder à la valeur des éléments inputs
+    let firstValue = document.getElementById("first").value;
+    let lastValue = document.getElementById("last").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    // j'affiche les résultats dans la console
+    console.log(firstValue);
+    console.log(lastValue);
+    console.log(email);
+    console.log(message);
+    // j'indique que isSubmited est true
+    isSubmited = true;
+});
+
+
+// fonction ternaire : si isSubmit est true, reset du formulaire et fermeture de la modal. 
+// si c'est false alors: null est renvoyé.
+function submitForm(){
+    isSubmited ? (formElement.reset(), closeModal()) : null;
+}
+// function submitForm(){
+//     if(isSubmited){
+//         formElement.reset()
+//         closeModal();
+//     }
+// };
+
+
+// Ajout d'un écouteur d'événement pour la soumission du formulaire
+// lorsque l'événement se produit, la fonction submitForm est appelée
+formElement.addEventListener("submit", submitForm);
+// Ajout d'un écouteur d'événement au clic sur le bouton "contact"
+// lorsque l'événement se produit, la fonction displayModal est appelée
 contact_button.addEventListener("click", displayModal);
+// Ajout d'un écouteur d'événement au clic sur le bouton "fermer"
+// lorsque l'événement se produit, la fonction closeModal est appelée
 close_button.addEventListener("click", closeModal);
